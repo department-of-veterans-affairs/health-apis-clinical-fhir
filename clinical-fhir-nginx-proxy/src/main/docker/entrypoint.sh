bc07e417ed90405ee47dc133d8294c15bf985ca9
jhulbert <joshua.hulbert@libertyits.com>
2021-03-16 12:59:53 -0400

    Local testing is fun for everyone!

--- clinical-fhir-nginx-proxy/src/main/docker/entrypoint.sh
+++ clinical-fhir-nginx-proxy/src/main/docker/entrypoint.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 set -eo pipefail
-set -x
-
-test -n "${DU_AWS_BUCKET_NAME:-}"
-test -n "${DU_S3_FOLDER:-}"
+# ==================================================
 
 log() {
   echo "$(date --iso-8601=seconds) $1"
 }
 
+# ==================================================
+
 CONF_FILE=${NGINX_CONF:-nginx.conf}
 
 mkdir /nginx
 
-log "Pulling nginx configuration from s3..."
-aws s3 cp s3://$DU_AWS_BUCKET_NAME/$DU_S3_FOLDER/$CONF_FILE /nginx/nginx.conf
+if [ -n "${DU_AWS_BUCKET_NAME:-}" ] && [ -n "${DU_S3_FOLDER:-}" ] 
+then
+  log "Pulling nginx configuration from s3..."
+  aws s3 cp s3://${DU_AWS_BUCKET_NAME}/${DU_S3_FOLDER}/${CONF_FILE} /nginx/nginx.conf
+else
+  log "Using local nginx configuration..."
+  cp ${CONF_FILE} /nginx/nginx.conf
+fi
 
 log "Starting nginx..."
 nginx -c /nginx/nginx.conf
+
