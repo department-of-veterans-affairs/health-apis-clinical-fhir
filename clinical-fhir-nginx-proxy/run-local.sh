c3b8e5becb57d3de4708527dca174eb77a529178
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-05-12 10:45:45 -0400

    run-local.sh now supports loading secrets automatically and using the CHAPI deployment unit nginx conf file if nearby  (#15)

--- clinical-fhir-nginx-proxy/run-local.sh
+++ clinical-fhir-nginx-proxy/run-local.sh
@@ -1,55 +1,124 @@
 #!/usr/bin/env bash
-set -euo pipefail
-# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+#=============================================
 
 usage() {
   cat <<EOF
-  Usage:
-    ${0} <nginx-configuration-file>
+Usage:
+  ${0} [options]
+
+Options:
+  --debug            Turn on debug output
+  -h|--help          Display this menu
+  -n|--nginx-conf    Location of nginx-conf file to use
+  -s|--secrets-conf  Location of secrets file for environment substitution
 
 ${1}
 EOF
-  exit 1
+exit 1
 }
 
-# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+#=============================================
 
 init() {
-  WORKING_DIR=$(dirname $0)/..
-  cd ${WORKING_DIR}
+  set -euo pipefail
+
+  WORK="$(readlink -f $(dirname $0))"
+  cd ${WORK}
+
+  # Clinical-Fhir was renamed. In the unlikely scenario that someone is using the old name, allow override of the var.
+  DEPLOYMENT_UNIT="${DEPLOYMENT_UNIT:-health-apis-clinical-fhir-api-deployment}"
+
+  SECRETS_CONF="${WORK}/secrets.conf"
+  DEV_CONF="${WORK}/dev-nginx.conf"
 }
 
 main() {
-  local conf=${1:-}
+  ARGS=$(getopt -n $(basename ${0}) \
+      -l "debug,help,nginx-conf:,secrets-conf:" \
+      -o "hn:s:" -- "$@")
+  [ $? != 0 ] && usage
+  eval set -- "$ARGS"
+  while true
+  do
+    case "$1" in
+      --debug) set -x;;
+      -h|--help) usage "Just don't. Don't even. I can't.";;
+      -n|--nginx-conf) NGINX_CONF="$(readlink -f ${2})";;
+      -s|--secrets-conf) SECRETS_CONF="${2}";;
+      --) shift;break;;
+    esac
+    shift;
+  done
 
-  if [ -z "${conf:-}" ]; then usage "nginx-configuration-file location must be specified."; fi
-
-  local version=$(projectVersion)
-  echo "Found project version: ${version}"
-  if [ -z "$(docker images --filter=reference=vasdvp/health-apis-clinical-fhir-nginx-proxy:${version} -q)" ]
-  then
-    echo "Couldn't find docker image for ${version} locally... Rebuilding..."
-    mvn clean package -Prelease
+  # Source if found, else try anyway (file may have been pre-populated)
+  if [ -f "${SECRETS_CONF}" ]
+  then 
+    source ${SECRETS_CONF}
+  else
+    echo "Could not find file (${SECRETS_CONF}). Attempting to run anyway..."
   fi
 
-  docker run --rm --detach \
-    --name clinical-fhir-proxy \
-    --volume ${conf}:/local-nginx.conf \
-    --net="host" \
-    --env NGINX_CONF=/local-nginx.conf \
-    vasdvp/health-apis-clinical-fhir-nginx-proxy:${version}
+  if [ -z "${NGINX_CONF:-}" ]
+  then 
+    echo "Using default nginx configuration from deployment-unit."
+    useDefaultNginxConf
+  fi
+
+  cat ${NGINX_CONF} | replacePorts | envsubst > ${DEV_CONF}
+
+  local appVersion="$(determineProjectVersion)"
+  findDockerImageForVersion "${appVersion}"
+
+  runClinicalFhirNginxProxy "${appVersion}"
 }
 
-# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+#=============================================
 
-projectVersion() {
-  cat pom.xml \
+determineProjectVersion() {
+  cat ${WORK}/../pom.xml \
     | grep -A 3 '<artifactId>clinical-fhir-parent</artifactId>' \
     | grep version \
     | sed 's/.*<version>\(.*\)<[/]version>/\1/'
 }
 
-# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+findDockerImageForVersion() {
+  local version="${1:-}"
+
+  echo "Version: ${version}"
+  if [ -z "$(docker images --filter=reference=vasdvp/health-apis-clinical-fhir-nginx-proxy:${version} -q)" ]
+  then
+    echo "Couldn't find docker image for ${version} locally... Rebuilding..."
+    mvn clean package -Prelease
+  fi
+}
+
+replacePorts() {
+  cat | sed \
+    -e "/set.*dq.*/s/\${BLUE_LOAD_BALANCER_PORT}/${DQ_PROXY_PORT:-${BLUE_LOAD_BALANCER_PORT}}/" \
+    -e "/set.*vfq.*/s/\${BLUE_LOAD_BALANCER_PORT}/${VFQ_PROXY_PORT:-${BLUE_LOAD_BALANCER_PORT}}/"
+}
+
+runClinicalFhirNginxProxy() {
+  local version="${1:-}"
+
+  docker run --rm \
+    --name clinical-fhir-proxy \
+    --volume ${DEV_CONF}:/local-nginx.conf \
+    --net="host" \
+    --env NGINX_CONF=/local-nginx.conf \
+    vasdvp/health-apis-clinical-fhir-nginx-proxy:${version}
+}
+
+useDefaultNginxConf() {
+  local du=$(find "${SHANKTOPUS_WORKSPACE:-~/va}" -type d -name "${DEPLOYMENT_UNIT}")
+  if [ -z "${du:-}" ]; then usage "Couldn't find clinical-fhir deployment unit: ${DEPLOYMENT_UNIT}";fi
+  local conf="$(find ${du} -type f -name 'nginx.properties' | head -n +1)"
+  if [ ! -f "${conf:-}" ]; then usage "Couldn't find default nginx.conf file."; fi
+  NGINX_CONF="${conf}"
+}
+
+#=============================================
 
 init
 main $@
+
